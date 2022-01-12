3561e3cc005bfa5fb53dd9ecd2590db4004e9b5f
agrimaldo <agrimaldo.brandao@dasa.com.br>
2022-01-12 10:55:26 -0300

    install cors

--- testing-types-api/src/main.ts
+++ testing-types-api/src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  //app.use(cors());
-  
+  app.use(cors());
   await app.listen(3001);
 }
 bootstrap();
